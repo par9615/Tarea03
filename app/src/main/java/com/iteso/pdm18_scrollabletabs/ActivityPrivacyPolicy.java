cde593da0b5ebbd51c942ec20fb40f4f4aa8393f
Miguel Hernández <par9615@gmail.com>
2018-03-12 18:15:27 -0600

    Internet permission fixed

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
@@ -1,26 +1,36 @@
 package com.iteso.pdm18_scrollabletabs;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-public class ActivityPrivacyPolicy extends AppCompatActivity {
+public class ActivityPrivacyPolicy extends AppCompatActivity implements DialogInterface.OnKeyListener {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
 
-        WebView webView = findViewById(R.id.activity_privacy_policy_webview);
-
+        webView = findViewById(R.id.activity_privacy_policy_webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        webView.setWebViewClient(new MyWebViewClient());
         webView.loadUrl("file:///android_asset/PrivacyPolicy.html");
+
     }
 
     public class WebAppInterface {
@@ -34,6 +44,33 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history,
+        // bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
 
 
